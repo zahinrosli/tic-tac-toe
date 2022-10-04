1e1133415796fcffb046013a62be0acd29a3a748
zahin <zahinrosli97@gmail.com>
2022-10-04 18:46:49 +0800

    Update and rename tic-tac-toe.py to Main Game.py

--- Main Game.py
+++ Main Game.py
@@ -1,4 +1,4 @@
-#  A simple Tic-Tac-Toe game
+# A simple Tic-Tac-Toe game
 # Players 'X' and 'O' take turn inputing their position on the command line using numbers 1-9
 # 1 | 2 | 3
 # ---------
@@ -15,15 +15,14 @@ board = {
     7: ' ', 8: ' ', 9: ' '
 }
 
-# TODO: update the gameboard with the user input
+# Update the gameboard with the user input
 def markBoard(position, mark):
     position = int(position)
     board[position] = mark
     return
 
 
-# TODO: print the game board as described at the top of this code skeleton
-# Will not be tested in Part 1
+# Print the game board as described at the top of this code skeleton
 def printBoard():
     print(board[1],"|", board[2] ,"|", board[3])
     print("---------")
@@ -33,10 +32,10 @@ def printBoard():
     return
 
 
-# TODO: check for wrong input, this function should return True or False.
+# Check for wrong input, this function should return True or False.
 # True denoting that the user input is correct
-# you will need to check for wrong input (user is entering invalid position) or position is out of bound
-# another case is that the position is already occupied
+# Need to check for wrong input (user is entering invalid position) or position is out of bound
+# Another case is that the position is already occupied
 def validateMove(position):
     position = int(position)
     if position >= 1 and position <= 9:
@@ -48,8 +47,7 @@ def validateMove(position):
         result = False
     return result
 
-# TODO: list out all the combinations of winning, you will neeed this
-# one of the winning combinations is already done for you
+# List out all the winning combinations
 winCombinations = [
     [1, 2, 3],
     [4, 5, 6],
@@ -61,7 +59,7 @@ winCombinations = [
     [3, 5, 7]
 ]
 
-# TODO: implement a logic to check if the previous winner just win
+# Implement a logic to check if the previous winner just win
 # This method should return with True or False
 def checkWin(player):
     if board[1] == board[2] == board[3] == player:
@@ -85,7 +83,7 @@ def checkWin(player):
     return result
 
 
-# TODO: implement a function to check if the game board is already full
+# Implement a function to check if the game board is already full
 # For tic-tac-toe, tie bascially means the whole board is already occupied
 # This function should return with boolean
 def checkFull():
@@ -94,16 +92,12 @@ def checkFull():
             return False
     return True
 
-
-#########################################################
-## Copy all your code/fucntions in Part 1 to above lines
-## (Without Test Cases)
 #########################################################
 
 gameEnded = False
 currentTurnPlayer = 'X'
 
-# entry point of the whole program
+# Entry point of the whole program
 print('Game started: \n\n' +
     ' 1 | 2 | 3 \n' +
     ' --------- \n' +
@@ -111,8 +105,7 @@ print('Game started: \n\n' +
     ' --------- \n' +
     ' 7 | 8 | 9 \n')
 
-# TODO: Complete the game play logic below
-# You could reference the following flow
+# Game play logic:
 # 1. Ask for user input and validate the input
 # 2. Update the board
 # 3. Check win or tie situation
@@ -168,8 +161,7 @@ def mainGame():
 mainGame()
 replay()
 
-# Bonus Point: Implement the feature for the user to restart the game after a tie or game over
-
+# Implement the feature for the user to restart the game after a tie or game over
 def replay():
     board_keys = []
     
